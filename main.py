eaa1efcd34fe19550d6eac54c59f68cb0017314e
papykabukanyi <papykabukanyi@gmail.com>
2023-11-09 12:05:06 -0700

    adding liveresume html and others minor chgs

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/liveresume.html")
+def liveresume():
+    return render_template("liveresume.html")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
