0172e8643cbe1eead3bfab37a49a781d88bb9ae6
papykabukanyi <papykabukanyi@gmail.com>
2024-04-12 07:01:42 -0700

    updated website toughtout

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## Papy Kabukanyi web dev Portfolio
+# Papy Kabukanyi web dev Portfolio
+
+This web portfolio was built using a pretemplate from [free-css](https://www.free-css.com/)
+
--- main.py
+++ main.py
@@ -3,19 +3,38 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
 
+@app.route("/works.html")
+def works():
+    return render_template("works.html")
 
-@app.route("/liveresume.html")
-def liveresume():
-    return render_template("liveresume.html")
+@app.route("/work.html")
+def work():
+    return render_template("work.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/components.html")
+def components():
+    return render_template("components.html")
+
+# @app.route("/liveresume.html")
+# def liveresume():
+#     return render_template("liveresume.html")
 
 
-@app.route("/contactsent.html")
-def contactsent():
-    return render_template("contactsent.html")
+
+
+
 
 
 if __name__ == "__main__":
