5a63b2715687461830c3fb6bc44985205b848a72
papykabukanyi <papykabukanyi@gmail.com>
2024-04-16 07:08:46 -0700

    added smtp

---
+++ config.py
@@ -0,0 +1,5 @@
+# Email configuration
+SMTP_SERVER = 'smtp-relay.brevo.com'
+SMTP_PORT = 587
+EMAIL_ADDRESS = 'papykabukanyi@gmail.com'
+EMAIL_PASSWORD = 'kVmRbzZMTOaGx4JA'
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,23 +19,27 @@ def work():
 def about():
     return render_template("about.html")
 
-@app.route("/contact.html")
-def contact():
-    return render_template("contact.html")
+# @app.route("/contact.html")
+# def contact():
+#     return render_template("contact.html")
 
 @app.route("/resume.html")
 def resume():
     return render_template("resume.html")
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        # Process form data (e.g., store in database, send email, etc.)
+        return 'Form submitted successfully!'
+    return render_template('contact.html')
+
 # @app.route("/liveresume.html")
 # def liveresume():
 #     return render_template("liveresume.html")
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
